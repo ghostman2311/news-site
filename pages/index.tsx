53854803273598f0e10ba453db3cb93e10f00156
Nikhil <nkarnwal07@gmail.com>
2023-05-07 14:38:33 +0530

    root page added

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+function Front() {
+  return (
+    <>
+      <Head>
+        <title>Front Page of the internet</title>
+      </Head>
+      <main>Hello World from Next!</main>
+    </>
+  );
+}
+
+export default Front;
